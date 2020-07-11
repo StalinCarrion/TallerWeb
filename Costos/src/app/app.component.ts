8639370bd74ce37d55ed4c503d35655cdc6b2150
StalinCarrion <stalinfernandocarrioncarchi96@hotmail.com>
2020-07-10 20:45:55 -0500

    taller costos

---
+++ Costos/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'cotizador Web';
+}
+
+var div = document.getElementById("inputEmail4");
+console.log(div);
+
+
